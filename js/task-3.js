af9fbcac651b49418038fe6ff670fed8967ab139
RomanHlushko <romaglusko346@gmail.com>
2024-04-13 20:07:31 +0300

    start HW 6

---
+++ js/task-3.js
@@ -0,0 +1,39 @@
+console.log('\n');
+
+class StringBuilder {
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padStart(str) {
+    this.#value = str.concat(this.#value);
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
+}
+
+const builder = new StringBuilder('.');
+
+console.log(builder.getValue()); // "."
+
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
